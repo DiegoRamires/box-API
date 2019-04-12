c60b5bd213f9600b63dd873099fdaf4fb7479fd5
DiegoRamires <goncalves.ramires@gmail.com>
2019-04-12 00:42:56 -0300

    configure multer to allow send files into database. add file `post` and `get` routes, and configure association between box and files

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -5,6 +5,14 @@ class BoxController {
     const box = await Box.create(req.body)
     return res.json(box)
   }
+
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: 'files',
+      options: { sort: { createdAt: -1 }}
+    })
+    return res.json(box)
+  }
 }
 
 module.exports = new BoxController()
